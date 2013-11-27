def4e7b631ba99ff6cc60ac2b5db2b285d6b0875
Brian Le <blsymph@gmail.com>
2013-11-27 11:40:44 +0000

    Add nested completion

--- mix-completion.bash
+++ mix-completion.bash
@@ -4,7 +4,7 @@
 
 _mix()
 {
-    local cwd second current previous more_previous exs tasks separator task_list task_list_file
+    local cwd current previous more_previous exs tasks separator task_list task_list_file superoption i j
 
     COMPREPLY=()
 
@@ -13,9 +13,9 @@ _mix()
     current="${COMP_WORDS[COMP_CWORD]}"
     previous="${COMP_WORDS[COMP_CWORD - 1]}"
     more_previous="${COMP_WORDS[COMP_CWORD - 2]}"
-    second="${COMP_WORDS[1]}"
 
     task_list_file="task_list"
+
     # create a cache if it doesn't exist
     if [ ! -f "$task_list_file" ] ; then
         # tasks.exs outputs a space-delimted string of Mix tasks
@@ -31,37 +31,64 @@ _mix()
         echo "$task_list" >> "$task_list_file"
     else
         task_list=$(head -1 "$task_list_file")
+        tasks=($task_list)
     fi
 
     case "${previous}" in
-        mix)                mix_complete_tasks; return 0;;
-        archive)            mix_complete_custom "--no-compile"; return 0;;
-            -i)                 mix_complete_directories '!*'; return 0;;
-            -o)                 mix_complete_files; return 0;;
-        clean)              mix_complete_custom "--all"; return 0;;
+        mix)
+            mix_complete_tasks
+            ;;
+        archive)
+            mix_complete_custom "${previous}"
+            ;;
+            -i)
+                mix_complete_directories
+                ;;
+
+            -o)
+                mix_complete_files
+                ;;
+        clean)              mix_complete_wordlist "--all"; return 0;;
         cmd)                mix_complete_commands; return 0;;
-        compile)            mix_complete_multi "--list"; return 0;;
+        compile)            mix_complete_wordlist"--list"; return 0;;
         # mix_complete_deps
-        deps.clean)         mix_complete_custom "--all --unlock"; return 0;;
-        deps.compile)       mix_complete_custom "--quiet"; return 0;;
-        deps.get)           mix_complete_custom "--no-compile --no-deps-check --quiet"; return 0;;
+        deps.clean)         mix_complete_custom "${previous}"; return 0;;
+        deps.compile)       mix_complete_wordlist "--quiet"; return 0;;
+        deps.get)           mix_complete_custom "${previous}"; return 0;;
         # mix_complete_deps
-        deps.unlock)        mix_complete_custom "--all"; return 0;;
+        deps.unlock)        mix_complete_wordlist "--all"; return 0;;
         # mix_complete_deps
-        deps.update)        mix_complete_custom "-- all --no-compile --no-deps-check --quiet"; return 0;;
+        deps.update)        mix_complete_custom "${previous}"; return 0;;
         do)                 mix_complete_tasks; return 0;;
-        escriptize)         mix_complete_custom "--force --no-compile"; return 0;;
-        local.install)      mix_complete_custom "--force"; return 0;;
+        escriptize)         mix_complete_custom "${previous}"; return 0;;
+        local.install)      mix_complete_wordlist"--force"; return 0;;
         local.rebar)        mix_complete_files; return 0;;
         local.uninstall)    mix_complete_tasks; return 0;;
         #fix
         help)               if [[ ! ${more_previous} == "help" ]] ; then mix_complete_tasks; fi; return 0;;
-        new)                mix_complete_multi "--bare --module --umbrella"; return 0;;
-        run)                mix_complete_multi "--eval --require --parallel-require, --no-halt, --no-compile, --no-start"; return 0;;
-        test)               mix_complete_multi "--trace --max-cases --cover --force --no-compile --no-start --no-color"; return 0;;
+        new)                mix_complete_custom "${previous}"; return 0;;
+        run)                mix_complete_custom "${previous}"; return 0;;
+        test)               mix_complete_custom "${previous}"; return 0;;
             --cover)            mix_complete_directories; return 0;;
         *)
-            if [[ ("${second}" == "do" && "${previous:(-1)}" == ",") ]] ; then mix_complete_tasks; fi
+            # retrive the last superoption, i.e. the last option that is not a suboption
+            for (( i=${#COMP_WORDS[@]} - 1; i >= 0; i-- ))
+            do
+                for (( j=${#tasks[@]} - 1; j >= 0; j-- ))
+                do
+                    if [[ "${COMP_WORDS[i]}" == "${tasks[j]}"  ]] ; then
+                        superoption="${tasks[j]}"
+                        break 2
+                    fi
+                done
+            done
+
+            if [[ ("${COMP_WORDS[1]}" == "do" && "${previous:(-1)}" == ",") ]] ; then
+                mix_complete_tasks
+            elif [[ ! -z "$superoption" && ! "$superoption" == "${current}" ]] ; then
+                mix_complete_custom "$superoption"
+            fi
+
             return 0;;
     esac
 
@@ -71,10 +98,42 @@ _mix()
 get_task_list()    { echo "what"; }
 
 mix_complete_commands()     { COMPREPLY=($(compgen -c -- ${current})); }
-mix_complete_custom()       { if [[ ${current} == --* ]] ; then COMPREPLY=($(compgen -W "${1}" -- ${current})); fi }
-mix_complete_directories()  { COMPREPLY=($(compgen -d -X -- ${current})); }
-mix_complete_files()        { COMPREPLY=($(compgen -f -X -- ${current})); }
+mix_complete_custom()
+{
+    case $1 in
+        archive)
+            mix_complete_wordlist "--no-compile"
+            ;;
+        deps.clean)
+            mix_complete_wordlist "--all --unlock";
+            ;;
+        deps.get)
+            mix_complete_wordlist "--no-compile --no-deps-check --quiet"
+            ;;
+        deps.update)
+            mix_complete_wordlist "-- all --no-compile --no-deps-check --quiet"
+            ;;
+        escriptize)
+            mix_complete_wordlist "--force --no-compile"
+            ;;
+        new)
+            mix_complete_multi "--bare --module --umbrella"
+            ;;
+        run)
+            mix_complete_multi "--eval --require --parallel-require --no-halt --no-compile --no-start"
+            ;;
+        test)
+            mix_complete_multi "--trace --max-cases --cover --force --no-compile --no-start --no-color"
+            ;;
+        *)
+            ;;
+    esac
+
+}
+mix_complete_directories()  { COMPREPLY=($(compgen -d -- ${current})); }
+mix_complete_files()        { COMPREPLY=($(compgen -f -- ${current})); }
 mix_complete_tasks()        { COMPREPLY=($(compgen -W "${task_list}" -- ${current})); }
 mix_complete_multi()        { if [[ ${current} == --* ]] ; then COMPREPLY=($(compgen -W "${1}" -- ${current})); else mix_complete_files; fi }
+mix_complete_wordlist()     { if [[ ${current} == --* ]] ; then COMPREPLY=($(compgen -W "${1}" -- ${current})); fi }
 
 complete -F _mix mix
--- tasks.exs
+++ tasks.exs
@@ -4,6 +4,6 @@ Mix.Task.load_all
 
 Mix.Task.all_modules
 |>  Enum.filter(fn(x) -> not Mix.Task.hidden?(x) end)
+|>  Enum.sort
 |>  Enum.map_join(" ", fn(x) -> Mix.Task.task_name(x) end)
 |>  IO.write
-# lc module inlist Mix.Task.all_modules, not Mix.Task.hidden?(module), do: IO.puts Mix.Task.task_name(module)
